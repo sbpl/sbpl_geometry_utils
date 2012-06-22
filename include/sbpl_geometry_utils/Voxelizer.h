844eab3265ceba7c3f7cae1fb143e064c3382c93
Andrew Dornbush <andrew.dornbush@gmail.com>
2012-06-22 17:41:56 -0400

    Added test cpp file that I forgot to add

--- include/sbpl_geometry_utils/Voxelizer.h
+++ include/sbpl_geometry_utils/Voxelizer.h
@@ -158,7 +158,8 @@ public:
 private:
 	Voxelizer();
 
-	static void createVoxelMesh(std::vector<Triangle>& triangles, std::vector<int>& indices);
+	static void createVoxelMesh(double res, const std::vector<int>& minVoxel, const std::vector<int>& maxVoxel,
+	                            std::vector<geometry_msgs::Point>& vertices, std::vector<int>& indices);
 	static void createSphereMesh(const std::vector<double>& sphere, int numLongitudes, int numLatitudes,
                                  std::vector<geometry_msgs::Point>& vertices, std::vector<int>& triangles);
 	static bool getAxisAlignedBoundingBox(const std::vector<geometry_msgs::Point>& vertices, double& minX,
