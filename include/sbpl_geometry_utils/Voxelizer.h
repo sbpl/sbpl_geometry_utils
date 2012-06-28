251057a1ca3675ee980f988c8b9428370526fbb6
Andrew Dornbush <andrew.dornbush@gmail.com>
2012-06-28 13:06:47 -0400

    corrected indentation so doxygen doesn't look stupid

--- include/sbpl_geometry_utils/Voxelizer.h
+++ include/sbpl_geometry_utils/Voxelizer.h
@@ -24,8 +24,8 @@ public:
      *                    this vector is a vector of doubles with the first three elements as the voxel's x,y,z
      *                    position and the fourth element as the length of a voxel's side
      */
-	static void voxelizeBox(double xSize, double ySize, double zSize, double voxelSize,
-	                        std::vector<std::vector<double> >& voxels);
+    static void voxelizeBox(double xSize, double ySize, double zSize, double voxelSize,
+                            std::vector<std::vector<double> >& voxels);
 
     /**
      * @brief Encloses a generic box with a set of voxels of a given size.
@@ -39,8 +39,8 @@ public:
      *                    this vector is a vector of doubles with the first three elements as the voxel's x,y,z
      *                    position and the fourth element as the length of a voxel's side
      */
-	static void voxelizeBox(double xSize, double ySize, double zSize, const geometry_msgs::Pose& pose, double voxelSize,
-	                        std::vector<std::vector<double> >& voxels);
+    static void voxelizeBox(double xSize, double ySize, double zSize, const geometry_msgs::Pose& pose, double voxelSize,
+                            std::vector<std::vector<double> >& voxels);
 
 	/**
      * @brief Encloses a sphere with a set of voxels of a given size.
@@ -53,7 +53,7 @@ public:
      *               this vector is a vector of doubles with the first three elements as the voxel's x,y,z
      *               position and the fourth element as the length of a voxel's side
      */
-	static void voxelizeSphere(double radius, double voxelSize, std::vector<std::vector<double> >& voxels);
+    static void voxelizeSphere(double radius, double voxelSize, std::vector<std::vector<double> >& voxels);
 
     /**
      * @brief Encloses a sphere with a set of voxels of a given size.
@@ -67,8 +67,8 @@ public:
      *               this vector is a vector of doubles with the first three elements as the voxel's x,y,z
      *               position and the fourth element as the length of a voxel's side
      */
-	static void voxelizeSphere(double radius, const geometry_msgs::Pose& pose, double voxelSize,
-	                           std::vector<std::vector<double> >& voxels);
+    static void voxelizeSphere(double radius, const geometry_msgs::Pose& pose, double voxelSize,
+                               std::vector<std::vector<double> >& voxels);
 
     /**
      * @brief Encloses an axis-aligned cylinder with a set of voxels of a given size.
@@ -82,41 +82,41 @@ public:
      *                    this vector is a vector of doubles with the first three elements as the sphere's x,y,z
      *                    position and the fourth element as the radius of the sphere
      */
-	static void voxelizeCylinder(double cylinderRadius, double length, double voxelSize,
-	                             std::vector<std::vector<double> >& voxels);
+    static void voxelizeCylinder(double cylinderRadius, double length, double voxelSize,
+                                 std::vector<std::vector<double> >& voxels);
 
-	/**
-	 * @brief Encloses a generic cylinder with a set of voxels of a given radius
-	 *
-	 * Encloses a cylinder with a set of spheres of a given radius. The cylinder is allowed any generic pose.
-	 *
-	 * @param[in] cylinderRadius,length The dimensions of the cylinder
-	 * @param[in] pose The position and rotation of the cylinder
-	 * @param[in] voxelSize The desired length of the generated voxels
-	 * @param[out] voxels The vector in which to store the spheres; any previous data is overwritten; each element of
+    /**
+     * @brief Encloses a generic cylinder with a set of voxels of a given radius
+     *
+     * Encloses a cylinder with a set of spheres of a given radius. The cylinder is allowed any generic pose.
+     *
+     * @param[in] cylinderRadius,length The dimensions of the cylinder
+     * @param[in] pose The position and rotation of the cylinder
+     * @param[in] voxelSize The desired length of the generated voxels
+     * @param[out] voxels The vector in which to store the spheres; any previous data is overwritten; each element of
      *                    this vector is a vector of doubles with the first three elements as the sphere's x,y,z
      *                    position and the fourth element as the radius of the sphere
-	 */
-	static void voxelizeCylinder(double cylinderRadius, double length, const geometry_msgs::Pose& pose,
-	                             double voxelSize, std::vector<std::vector<double> >& voxels);
+     */
+    static void voxelizeCylinder(double cylinderRadius, double length, const geometry_msgs::Pose& pose,
+                                 double voxelSize, std::vector<std::vector<double> >& voxels);
 
-	/**
-	 * @brief Encloses a mesh with a set of voxels of a given size
-	 *
-	 * Encloses a mesh with a set of voxels of a given size. The generated voxels appear in whatever frame the vertices
-	 * and triangles of the mesh are defined in.
-	 *
-	 * @param[in] vertices The vertices of the mesh
-	 * @param[in] triangles The triangles of the mesh as a list of indices into the vertex array
-	 * @param[in] voxelSize The desired length of the generated voxels
-	 * @param[out] voxels The vector in which to store the spheres; any previous data is overwritten; each element of
+    /**
+     * @brief Encloses a mesh with a set of voxels of a given size
+     *
+     * Encloses a mesh with a set of voxels of a given size. The generated voxels appear in whatever frame the vertices
+     * and triangles of the mesh are defined in.
+     *
+     * @param[in] vertices The vertices of the mesh
+     * @param[in] triangles The triangles of the mesh as a list of indices into the vertex array
+     * @param[in] voxelSize The desired length of the generated voxels
+     * @param[out] voxels The vector in which to store the spheres; any previous data is overwritten; each element of
      *                    this vector is a vector of doubles with the first three elements as the sphere's x,y,z
      *                    position and the fourth element as the radius of the sphere
-	 * @param[in] fillMesh Whether to fill the interior of the mesh with spheres
-	 * @param[in] maxVoxels An upper bound on the number of generated spheres; will use a larger voxel size than
-	 *                      $voxelSize if necessary to reduce the number of voxels generated
-	 */
-	static void voxelizeMesh(const std::vector<geometry_msgs::Point>& vertices, const std::vector<int>& triangles,
+     * @param[in] fillMesh Whether to fill the interior of the mesh with spheres
+     * @param[in] maxVoxels An upper bound on the number of generated spheres; will use a larger voxel size than
+     *                      $voxelSize if necessary to reduce the number of voxels generated
+     */
+    static void voxelizeMesh(const std::vector<geometry_msgs::Point>& vertices, const std::vector<int>& triangles,
                              double voxelSize, std::vector<std::vector<double> >& voxels, bool fillMesh = false,
                              int maxVoxels = 0);
 
@@ -137,50 +137,50 @@ public:
      * @param[in] maxVoxels An upper bound on the number of generated spheres; will use a larger voxel size than
      *                      $voxelSize if necessary to reduce the number of voxels generated
      */
-	static void voxelizeMesh(const std::vector<geometry_msgs::Point>& vertices, const std::vector<int>& triangles,
+    static void voxelizeMesh(const std::vector<geometry_msgs::Point>& vertices, const std::vector<int>& triangles,
                              const geometry_msgs::Pose& pose, double voxelSize,
                              std::vector<std::vector<double> >& voxels, bool fillMesh = false, int maxSpheres = 0);
 
-	/**
-	 * @brief Encloses a list of spheres with a set of voxels of a given size
-	 *
-	 * Encloses a list of spheres with a set of voxels of a given size. The generated voxels appear in the frame the
-	 * spheres are described in.
-	 *
-	 * @param[in] spheres The list of spheres to voxelize
-	 * @param[in] res The resolution of the voxel cells
-	 * @param[in] removeDuplicates Whether to remove duplicate voxels
-	 * @param[out] voxels The vector in which to store the voxels
-	 * @param[out] volume The combined volume of all the spheres
-	 */
-	static void voxelizeSphereList(const std::vector<std::vector<double> >& spheres, double res, bool removeDuplicates,
-	                               std::vector<std::vector<double> >& voxels, double& volume);
+    /**
+     * @brief Encloses a list of spheres with a set of voxels of a given size
+     *
+     * Encloses a list of spheres with a set of voxels of a given size. The generated voxels appear in the frame the
+     * spheres are described in.
+     *
+     * @param[in] spheres The list of spheres to voxelize
+     * @param[in] res The resolution of the voxel cells
+     * @param[in] removeDuplicates Whether to remove duplicate voxels
+     * @param[out] voxels The vector in which to store the voxels
+     * @param[out] volume The combined volume of all the spheres
+     */
+    static void voxelizeSphereList(const std::vector<std::vector<double> >& spheres, double res, bool removeDuplicates,
+                                   std::vector<std::vector<double> >& voxels, double& volume);
 
-	/**
-	 * @brief a Quick And Dirty (QAD) enclosure of a list of spheres with a set of voxels of a given size
-	 *
-	 * Encloses a list of spheres with a set of voxels of a given size. The generated voxels appear in the frame the
-	 * spheres are described in.
-	 *
-	 * @param[in] spheres The list of spheres to voxelize
-	 * @param[in] res The resolution of the voxel cells
-	 * @param[in] removeDuplicates Whether to remove duplicate voxels
-	 * @param[out] voxels The vector in which to store the voxels
-	 * @param[out] volume The combined volume of all the spheres
-	 */
-  static void voxelizeSphereListQAD(const std::vector<std::vector<double> >& spheres, double res, bool removeDuplicates,
-                                 std::vector<std::vector<double> >& voxels, double& volume);
+    /**
+     * @brief a Quick And Dirty (QAD) enclosure of a list of spheres with a set of voxels of a given size
+     *
+     * Encloses a list of spheres with a set of voxels of a given size. The generated voxels appear in the frame the
+     * spheres are described in.
+     *
+     * @param[in] spheres The list of spheres to voxelize
+     * @param[in] res The resolution of the voxel cells
+     * @param[in] removeDuplicates Whether to remove duplicate voxels
+     * @param[out] voxels The vector in which to store the voxels
+     * @param[out] volume The combined volume of all the spheres
+     */
+    static void voxelizeSphereListQAD(const std::vector<std::vector<double> >& spheres, double res, bool removeDuplicates,
+                                      std::vector<std::vector<double> >& voxels, double& volume);
 
 private:
-	Voxelizer();
+    Voxelizer();
 
-	static void createVoxelMesh(double res, const std::vector<int>& minVoxel, const std::vector<int>& maxVoxel,
-	                            std::vector<geometry_msgs::Point>& vertices, std::vector<int>& indices);
-	static void createSphereMesh(const std::vector<double>& sphere, int numLongitudes, int numLatitudes,
+    static void createVoxelMesh(double res, const std::vector<int>& minVoxel, const std::vector<int>& maxVoxel,
+                                std::vector<geometry_msgs::Point>& vertices, std::vector<int>& indices);
+    static void createSphereMesh(const std::vector<double>& sphere, int numLongitudes, int numLatitudes,
                                  std::vector<geometry_msgs::Point>& vertices, std::vector<int>& triangles);
-	static bool getAxisAlignedBoundingBox(const std::vector<geometry_msgs::Point>& vertices, double& minX,
+    static bool getAxisAlignedBoundingBox(const std::vector<geometry_msgs::Point>& vertices, double& minX,
                                           double& minY, double& minZ, double& maxX, double& maxY, double& maxZ);
-	static void createCubeMesh(double x, double y, double z, double length, std::vector<Triangle>& trianglesOut);
+    static void createCubeMesh(double x, double y, double z, double length, std::vector<Triangle>& trianglesOut);
     static bool isInDiscreteBoundingBox(int i, int j, int k, int minx, int miny, int minz,
                                         int maxx, int maxy, int maxz);
     static bool intersects(const Triangle& tr1, const Triangle& tr2, double eps = 1.0e-4);
