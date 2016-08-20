e5a0c22d1b2b69457cfb1209c9b9ad69e98c269f
Andrew Dornbush <andrew.dornbush@gmail.com>
2016-08-20 12:07:40 -0400

    swap msg type for eigen in sphere

--- include/sbpl_geometry_utils/Sphere.h
+++ include/sbpl_geometry_utils/Sphere.h
@@ -1,10 +1,10 @@
 //////////////////////////////////////////////////////////////////////////////
 // Copyright (c) 2014, Andrew Dornbush
 // All rights reserved.
-// 
+//
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
-// 
+//
 //     * Redistributions of source code must retain the above copyright
 //       notice, this list of conditions and the following disclaimer.
 //     * Redistributions in binary form must reproduce the above copyright
@@ -13,7 +13,7 @@
 //     * Neither the name of the copyright holder nor the names of its
 //       contributors may be used to endorse or promote products derived from
 //       this software without specific prior written permission.
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 // AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 // IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -27,23 +27,24 @@
 // POSSIBILITY OF SUCH DAMAGE.
 //////////////////////////////////////////////////////////////////////////////
 
-#ifndef SBPL_GEOMETRY_UTILS_SPHERE_H
-#define SBPL_GEOMETRY_UTILS_SPHERE_H
+#ifndef sbpl_geometry_sphere_h
+#define sbpl_geometry_sphere_h
 
-#include <geometry_msgs/Point.h>
+#include <Eigen/Dense>
 
-namespace sbpl
-{
+namespace sbpl {
 
 class Sphere
 {
 public:
-    geometry_msgs::Point center;
-    double radius;
 
-    Sphere();
-    Sphere(const geometry_msgs::Point& center, double radius);
-    ~Sphere();
+    Eigen::Vector3d c;
+    double r;
+
+    Sphere() : c(Eigen::Vector3d::Zero()), r(1.0) { }
+    Sphere(const Eigen::Vector3d& c) : c(c), r(1.0) { }
+    Sphere(double r) : c(Eigen::Vector3d::Zero()), r(r) { }
+    Sphere(const Eigen::Vector3d& c, double r) : c(c), r(r) { }
 };
 
 }
